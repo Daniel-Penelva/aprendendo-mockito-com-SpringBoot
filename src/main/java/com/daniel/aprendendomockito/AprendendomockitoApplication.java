e9e0160975078b225a67428d1b27bcdd3bd0438f
Daniel Penelva <d4n.andrade@gmail.com>
2023-09-07 04:29:37 -0300

    Criando projeto Maven

---
+++ src/main/java/com/daniel/aprendendomockito/AprendendomockitoApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.aprendendomockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprendendomockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprendendomockitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daniel/aprendendomockito/AprendendomockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.aprendendomockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprendendomockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
