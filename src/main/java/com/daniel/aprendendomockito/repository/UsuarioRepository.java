5abaa2d0e88c6b9ff7c52e5d5de7326275329b08
Daniel Penelva <d4n.andrade@gmail.com>
2023-09-07 05:28:10 -0300

    Criando uma consulta personalizada pelo @Query utilizando o LIKE para buscar usuario por nome

--- src/main/java/com/daniel/aprendendomockito/repository/UsuarioRepository.java
+++ src/main/java/com/daniel/aprendendomockito/repository/UsuarioRepository.java
@@ -1,10 +1,15 @@
 package com.daniel.aprendendomockito.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import com.daniel.aprendendomockito.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
-    Usuario findByNome(String nome);
-    
+    @Query("SELECT u FROM Usuario u WHERE u.nome LIKE %:nome%")
+    List<Usuario> findByNome(@Param("nome") String nome);
 }
