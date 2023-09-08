99a7094ba22ffe17bac16a1b39cd221b3a948d4f
Daniel Penelva <d4n.andrade@gmail.com>
2023-09-07 21:40:10 -0300

    Alterando e add dependencias do JUnit5 (pom.xml) + criando testCriarUsuario

--- src/test/java/com/daniel/aprendendomockito/AprendendomockitoApplicationTests.java
+++ src/test/java/com/daniel/aprendendomockito/AprendendomockitoApplicationTests.java
@@ -1,13 +1,49 @@
 package com.daniel.aprendendomockito;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.daniel.aprendendomockito.model.Usuario;
+import com.daniel.aprendendomockito.repository.UsuarioRepository;
+import com.daniel.aprendendomockito.service.UsuarioService;
 
 @SpringBootTest
 class AprendendomockitoApplicationTests {
 
+	@Mock
+	private UsuarioRepository usuarioRepository;
+
+	@InjectMocks
+	private UsuarioService usuarioService;
+
+	@BeforeEach
+	public void setUp() {
+		MockitoAnnotations.openMocks(this);
+	}
+
 	@Test
-	void contextLoads() {
+	public void testCriarUsuario() {
+
+		// Criando um novo usuário
+		Usuario novoUsuario = new Usuario();
+		novoUsuario.setNome("Ana");
+
+		// Definindo o comportamento do Mock do repositório
+		when(usuarioRepository.save(any(Usuario.class))).thenReturn(novoUsuario);
+
+		// Chamando o método do serviço para criar o usuário
+		Usuario resultado = usuarioService.criarUsuario(novoUsuario);
+
+		// Verificando se o usuário é o mesmo usuário do exemplo
+		assertEquals(novoUsuario, resultado);
 	}
 
 }
